48b9ed61f8f1f1fde5d6988f75ca692ede4b05f0
ssarahahmed <sarahaahmed13@gmail.com>
2026-01-22 19:17:49 -0600

    Implemented main method for P3

--- src/Problem3.java
+++ src/Problem3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Problem3 {
     public static long[] NotFibonacci(int n){
         long[] sequence = new long[n];
@@ -9,5 +11,19 @@ public class Problem3 {
             sequence[i] = (long) Math.floor(1.5 * sequence[i -1] + 2 * sequence[i - 2]);
         }
         return sequence;
+    }
+
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+
+        long[] sequence = NotFibonacci(n);
+
+        System.out.println("NotFibonacci Sequence: ");
+        for (int i = 0; i < sequence.length; i++){
+            System.out.println(sequence[i]);
+        }
+        sc.close();
     }
 }
