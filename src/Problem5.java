330ff4b0e5a13b3e569cce41ea7de00ee55f6e2f
ssarahahmed <sarahaahmed13@gmail.com>
2026-01-22 19:59:57 -0600

    Implemented removeElement method in P5

--- src/Problem5.java
+++ src/Problem5.java
@@ -1,2 +1,12 @@
 public class Problem5 {
+    public static int removeElement(double[] nums, double val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
 }
