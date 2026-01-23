a069898f0230a53ee53fdcf1a03ba2e7009645b4
ssarahahmed <sarahaahmed13@gmail.com>
2026-01-22 18:16:51 -0600

    Implemented method for P1

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Problem1 {
+
+    public static int commonSubsequence( String s1, String s2, int m, int n) {
+        if (m==0 || n==0){
+            return 0;
+        }
+
+        if (s1.charAt(m-1) == s2.charAt(n-1)) {
+            return 1 + commonSubsequence(s1, s2, m-1, n-1);
+        } else{
+            int lcs1 = commonSubsequence(s1, s2, m-1, n);
+            int lcs2 = commonSubsequence(s1, s2, m, n-1);
+
+            if(lcs1 > lcs2){
+                return lcs1;
+            } else {
+                return lcs2;
+            }
+        }
+    }
+
 }
