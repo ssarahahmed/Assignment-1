75a83ff542a68f3af2fbf1be6691155583e45b27
ssarahahmed <sarahaahmed13@gmail.com>
2026-01-22 19:12:25 -0600

    Implemented NotFib method for P3

--- src/Problem3.java
+++ src/Problem3.java
@@ -1,2 +1,13 @@
 public class Problem3 {
+    public static long[] NotFibonacci(int n){
+        long[] sequence = new long[n];
+
+        if (n >= 1) sequence[0] = 0;
+        if (n >= 2) sequence[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            sequence[i] = (long) Math.floor(1.5 * sequence[i -1] + 2 * sequence[i - 2]);
+        }
+        return sequence;
+    }
 }
